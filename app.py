1fa4afe6389552b2bbe1a4fb5706cb2b8138044d
andreasprihananto2023 <andreas.prihananto2023@student.president.ac.id>
2024-12-12 00:53:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
     if 'page' not in st.session_state:
         st.session_state.page = "Home Page"
     if "sidebar_state" not in st.session_state:
-    st.session_state.sidebar_state = "collapsed"
+        st.session_state.sidebar_state = "collapsed"
 
     page = st.sidebar.radio("Pilih Halaman", ["Home Page", "Transformasi Gambar"], index=["Home Page", "Transformasi Gambar"].index(st.session_state.page))
     
