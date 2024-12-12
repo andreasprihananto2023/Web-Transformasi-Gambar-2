31c2a253dc81cef80d370181b9fbcdaa4ffa7d33
andreasprihananto2023 <andreas.prihananto2023@student.president.ac.id>
2024-12-12 14:52:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,11 +70,11 @@ def main():
         col1, col2, col3 = st.columns([1, 1, 1])
 
         with col1:
-            st.image("foto rizki.jpg", caption="Ahmad Rizki Safei", width=150)
+            st.image("foto rizki.jpg", caption="Ahmad Rizki Safei", width=160)
         with col2:
-            st.image("foto andre.jpg", caption="Andreas Prihananto", width=150)
+            st.image("foto andre.jpg", caption="Andreas Prihananto", width=160)
         with col3:
-            st.image("foto firdaus.jpg", caption="Firdaus Bachtiar", width=150)
+            st.image("foto firdaus.jpg", caption="Firdaus Bachtiar", width=160)
 
         st.write("")
         st.write("Klik tombol di bawah untuk mulai.")
