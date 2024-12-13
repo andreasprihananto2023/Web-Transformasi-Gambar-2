0e67b767ad546e757b7e5958fe53aee93e3dda7f
andreasprihananto2023 <andreas.prihananto2023@student.president.ac.id>
2024-12-13 20:32:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -264,10 +264,6 @@ def main():
             for key, value in metadata.items():
                 st.write(f"{key}: {value}")
 
-
-            # Tampilkan warna dominan
-            st.write(f"Warna dominan: {dominant_color}")
-            st.markdown(f"<div style='width: 100px; height: 100px; background-color: rgb({dominant_color[0]}, {dominant_color[1]}, {dominant_color[2]});'></div>", unsafe_allow_html=True)
     
             # Hapus warna non-dominan
             gambar_dominan = remove_non_dominant_colors(gambar_asli, dominant_color)
